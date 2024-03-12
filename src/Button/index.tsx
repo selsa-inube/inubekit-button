14fbb6b53d35c88d0034cb63c61e1ba2abcef46b
cmarin001 <cmarin6051@gmail.com>
2024-03-12 17:27:02 -0500

    fix(button): adjusment for hover event

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -6,6 +6,7 @@ import { inube } from "@inubekit/foundations";
 import { Appearance, Type, Spacing, Variant } from "./props";
 
 import { StyledButton, StyledLink } from "./styles";
+import { useState } from "react";
 
 interface IButton {
   children?: React.ReactNode;
@@ -24,6 +25,18 @@ interface IButton {
   parentHover?: boolean;
 }
 
+const determineParentHover = (
+  variant: string,
+  cursorHover: boolean,
+  isHovered: boolean,
+  parentHover: boolean,
+) => {
+  if (variant === "filled") {
+    return false;
+  }
+  return cursorHover && !parentHover ? isHovered : parentHover;
+};
+
 const ButtonStructure = (props: IButton) => {
   const {
     children,
@@ -41,6 +54,11 @@ const ButtonStructure = (props: IButton) => {
     parentHover = false,
   } = props;
 
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
   const interceptClick = (e: Event) => {
     try {
       onClick && onClick(e);
@@ -52,6 +70,13 @@ const ButtonStructure = (props: IButton) => {
       }
     }
   };
+
+  const helperParentHover = determineParentHover(
+    variant,
+    cursorHover,
+    isHovered,
+    parentHover,
+  );
 
   return (
     <StyledButton
@@ -67,6 +92,8 @@ const ButtonStructure = (props: IButton) => {
       onClick={disabled ? null : interceptClick}
       $cursorHover={cursorHover}
       $parentHover={parentHover}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       {loading && !disabled ? (
         <Spinner
@@ -91,8 +118,7 @@ const ButtonStructure = (props: IButton) => {
                   : appearance
               }
               disabled={disabled}
-              cursorHover={variant === "filled" ? false : cursorHover}
-              parentHover={variant === "filled" ? false : parentHover}
+              parentHover={helperParentHover}
             />
           )}
           <Text
@@ -105,8 +131,7 @@ const ButtonStructure = (props: IButton) => {
             }
             disabled={disabled}
             ellipsis={true}
-            cursorHover={variant === "filled" ? false : cursorHover}
-            parentHover={variant === "filled" ? false : parentHover}
+            parentHover={helperParentHover}
             textAlign="start"
           >
             {children}
@@ -122,8 +147,7 @@ const ButtonStructure = (props: IButton) => {
                   : appearance
               }
               disabled={disabled}
-              cursorHover={variant === "filled" ? false : cursorHover}
-              parentHover={variant === "filled" ? false : parentHover}
+              parentHover={helperParentHover}
             />
           )}
         </Stack>
